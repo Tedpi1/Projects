cb001fd5dc99436f3ce7afede04a62eef5d9b16a
tedtony <moroted16@gmail.com>
2024-05-16 19:25:18 +0300

    finnished

--- Reviews/index.js
+++ Reviews/index.js
@@ -1,34 +1,22 @@
-const reviews=[
+const review=[
     {
-    id: 1,
-    name: "susan smith",
-    job: "web developer",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883323/person-1_rfzsh.jpg",
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
-  {
-    id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883323/person-2_np9x5l.jpg",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-},
-
-{
-    id: 3,
-    name: "peter jones",
-    job: "intern",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883324/person-3_ipa0mj.jpg",   
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-  },
-  {
-      
-    id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883324/person-4_t9nxj.jpg",
-    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-  }
+        name:'Antoney Ocheing',
+        image:'p1.jpg',
+        job:'Frontend Developer',
+        info:'With the necessary equipments available, <br> I Antoney Ochieng will offer desire result to the organization'
+    },
+    {
+        name:'Francis Otieno',
+        image:'p2.jpg',
+        job:'Interior Designer',
+        info:'I am a professional interior designer with over 10 years of experience. <br> I will help you design your dream home'
+    },
+    {
+        name:'Joyce Wanjiru',
+        image:'p3.jpg',
+        job:'Graphic Designer',
+        info:'I am a professional graphic designer with over 10 years of experience. <br> I will help you design your dream website'
+    }
 ]
 const img=document.getElementById('person-img');
 const author=document.getElementById('author');
@@ -37,32 +25,42 @@ const info=document.getElementById('info');
 const prevBtn=document.querySelector('.prev-btn');
 const nextBtn=document.querySelector('.next-btn');
 const randomBtn=document.querySelector('.random-btn');
+
+// set starting item
 let currentItem=0;
 
+// load initial item
 window.addEventListener('DOMContentLoaded',function(){
     showPerson(currentItem);
 })
 
+// show person based on item
 function showPerson(person){
-    const item=reviews[person];
-    img.src=item.img;
+    const item=review[person];
+    img.src=item.image;
     author.textContent=item.name;
     job.textContent=item.job;
-    info.textContent=item.text;
+    info.textContent=item.info;
 }
 
+// show next person
 nextBtn.addEventListener('click',function(){
     currentItem++;
-    if(currentItem>reviews.length-1){
+    if(currentItem>review.length-1){
         currentItem=0;
     }
     showPerson(currentItem);
 })
-
-prevBtn.addEventListener('click',function(){
+//show the previous person
+prevBtn.addEventListener('clck',function(){
     currentItem--;
     if(currentItem<0){
-        currentItem=reviews.length-1;
+        currentItem=review.length-1;
     }
-    showPerson(currentItem);
+    showPerson(currentItem);    
 })
+//random person
+randomBtn.addEventListener('click',function(){
+    currentItem=Math.floor(Math.random()*review.length);
+    showPerson(currentItem);    
+})  
