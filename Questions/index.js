70d38771d70592615fb24bfe4ec1fa1642461d12
tedtony <moroted16@gmail.com>
2024-06-15 08:19:46 +0300

    Questions

---
+++ Questions/index.js
@@ -0,0 +1,24 @@
+/*const btn1 = document.querySelectorAll('.question-btn')
+btn1.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const question =e.currentTarget.parentElement.parentElement
+        question.classList.toggle('show-text')
+    })
+})*/
+
+
+
+
+//using selector inside the element
+const questions=document.querySelectorAll('.question');
+questions.forEach(function(question){
+    const btn=question.querySelector('.question-btn');
+    btn.addEventListener('click', function(){
+        questions.forEach(function (item){
+            if(item !== question){
+                item.classList.remove('show-text')
+            }
+        });
+        question.classList.toggle('show-text')
+    })
+})
